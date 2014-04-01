9f924424b7e471a8df46ad7a2fde0ca415b42640
Srinivas Gorur-Shandilya <code@srinivas.gs>
2014-04-01 16:57:40 -0400

    more options for prun

--- prun.m
+++ prun.m
@@ -1,8 +1,21 @@
 % prun
 % run jobs in parallel
-n = feature('numCores');
+function prun(n,algo)
+if nargin < 1
+	n = feature('numCores');
+end
+if nargin < 2
+	algo = 'GPU';
+end
+
+
 parfor i = 1:n
 	cd(strcat('fv_batch',mat2str(i)))
-	gpuTrack;
+	switch algo
+	case 'GPU'
+		gpuTrack;
+	case 'CPU'
+		cpuTrack;
+	end
 end
 delete(gcp)
